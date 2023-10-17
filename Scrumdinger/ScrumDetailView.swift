9c59446daaf5cc828ec6ad800fc339b6346cf4aa
Yacin Tmimi <yacintmimi@gmail.com>
2023-10-16 23:43:26 -0400

    Creating a navigation hierarchy
    In this tutorial, you'll create a navigation hierarchy. You'll start
    with the list screen and then present the detail view. Finally, you'll
    push the timer view to the top of the navigation stack.

---
+++ Scrumdinger/ScrumDetailView.swift
@@ -0,0 +1,54 @@
+//
+//  ScrumDetailView.swift
+//  Scrumdinger
+//
+//  Created by Yacin Tmimi on 10/16/23.
+//
+
+import SwiftUI
+
+struct ScrumDetailView: View {
+    let scrum: DailyScrum
+
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+        .navigationTitle(scrum.title)
+    }
+}
+
+struct ScrumDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            ScrumDetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
