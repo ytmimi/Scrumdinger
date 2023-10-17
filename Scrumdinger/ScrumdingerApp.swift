0d8bbb15e6da34246f4c06db9530bb5f1a5f2fba
Yacin Tmimi <yacintmimi@gmail.com>
2023-10-16 21:59:33 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yacin Tmimi on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
