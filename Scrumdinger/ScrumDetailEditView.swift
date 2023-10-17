96be1ae615df9a22460a70a304a0f07465fb724e
Yacin Tmimi <yacintmimi@gmail.com>
2023-10-17 00:50:14 -0400

    Creating the edit view
    In this tutorial, you'll create a new view containing controls to edit a
    scrum. SwiftUI provides all the standard controls for iOS apps. You'll
    add buttons, a slider, and a text field. Then, you'll update the detail
    view to present your new edit view.

---
+++ Scrumdinger/ScrumDetailEditView.swift
@@ -0,0 +1,61 @@
+//
+//  ScrumDetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Yacin Tmimi on 10/17/23.
+//
+
+import SwiftUI
+
+struct ScrumDetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
+
+
+
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(true)
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+
+                }
+            }
+        }
+    }
+}
+
+struct ScrumDetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumDetailEditView()
+    }
+}
