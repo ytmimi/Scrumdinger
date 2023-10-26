dce88548312b0100621554541ef84e166134ba7d
Yacin Tmimi <yacintmimi@gmail.com>
2023-10-25 22:52:36 -0400

    Managing state and life cycle
    In this tutorial, you'll use reference type models with SwiftUI views.
    You'll display meeting time and progress in views that better represent
    the final design.
    
    I was supposed to learn how to integrate AVFoundation to play audio
    when the scrum's state changes, but when I tried to follow along with
    the tutorial I got weird code signing issues and the app could no longer
    build so I opted to leave that feature out when following along with the
    tutorial.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -8,43 +8,41 @@
 import SwiftUI
 
 struct MeetingView: View {
-    var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
-                }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("600", systemImage: "hourglass.bottomhalf.fill")
-                }
-            }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time remaining")
-            .accessibilityValue("10 minutes")
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
 
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
-                }
-                .accessibilityLabel("Next speaker")
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(
+                    secondsElapsed: scrumTimer.secondsElapsed,
+                    secondsRemaining: scrumTimer.secondsRemaining,
+                    theme: scrum.theme
+                )
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                MeetingFooterView(
+                    speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker
+                )
             }
         }
         .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
